30e6ddcd5c2f97ca99a1a1bd16371ad3cf6f692c
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:55:17 +0530

    Update StringExample19.java

--- StringExample19.java
+++ StringExample19.java
@@ -1,1 +1,8 @@
+public class StringExample19 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String result = str.toLowerCase();
+        System.out.println("Lowercase string: " + result);
+    }
+}
 
