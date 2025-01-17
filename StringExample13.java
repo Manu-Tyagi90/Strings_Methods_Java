0ebea9aff6fec6e656dbe49147273dbfa5dee8a9
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:51:07 +0530

    Create StringExample13.java

---
+++ StringExample13.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String result = str.replaceAll("o", "a");
+        System.out.println("Replaced string: " + result);
+    }
+}
