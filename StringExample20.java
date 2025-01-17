41c8a7ad0a84ca2dc74ce565de382c6117ff9d91
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:56:25 +0530

    Create StringExample20.java

---
+++ StringExample20.java
@@ -0,0 +1,7 @@
+public class StringExample20 {
+    public static void main(String[] args) {
+        String str = "  Hello, World!  ";
+        String result = str.trim();
+        System.out.println("Trimmed string: " + result);
+    }
+}
