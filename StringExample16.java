cb2553f10c7566749d4af6108d8762bb1ad766ac
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:53:18 +0530

    Create StringExample16.java

---
+++ StringExample16.java
@@ -0,0 +1,7 @@
+public class StringExample16 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String result = str.substring(7);
+        System.out.println("Substring: " + result);
+    }
+}
