aa5ae07d21bf699fa561fca8b96290bafae42a97
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:54:15 +0530

    Create StringExample17.java

---
+++ StringExample17.java
@@ -0,0 +1,9 @@
+public class StringExample17 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char[] chars = str.toCharArray();
+        for (char ch : chars) {
+            System.out.println("Character: " + ch);
+        }
+    }
+}
