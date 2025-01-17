994ad75125ad23777a5afded60ff5d025be68e3c
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:44:37 +0530

    Create StringExample1.java

---
+++ StringExample1.java
@@ -0,0 +1,8 @@
+public class StringExample1 {
+    public static void main(String[] args) {
+        String str1 = "apple";
+        String str2 = "banana";
+        int result = str1.compareTo(str2);
+        System.out.println("Comparison result: " + result);
+    }
+}
