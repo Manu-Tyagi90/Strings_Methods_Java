c9a858d2b740827caaae929df1c71481b0384794
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:47:45 +0530

    Create StringExample7.java

---
+++ StringExample7.java
@@ -0,0 +1,6 @@
+public class StringExample7 {
+    public static void main(String[] args) {
+        String str = String.format("Hello, %s!", "World");
+        System.out.println("Formatted string: " + str);
+    }
+}
