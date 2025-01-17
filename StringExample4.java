10e7294765270f51d0166a7df6d53e345c01c281
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:46:03 +0530

    Create StringExample4.java

---
+++ StringExample4.java
@@ -0,0 +1,7 @@
+public class StringExample4 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        boolean result = str.endsWith("World!");
+        System.out.println("Ends with 'World!': " + result);
+    }
+}
