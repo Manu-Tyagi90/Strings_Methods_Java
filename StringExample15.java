fbe1842103bc1653bab58b1b0f74b04d57296d73
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:52:48 +0530

    Create StringExample15.java

---
+++ StringExample15.java
@@ -0,0 +1,7 @@
+public class StringExample15 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        boolean result = str.startsWith("Hello");
+        System.out.println("Starts with 'Hello': " + result);
+    }
+}
