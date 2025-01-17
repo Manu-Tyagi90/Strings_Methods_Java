151b9eae006414e16a2222fd03693078e5146d59
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:48:20 +0530

    Create StringExample8.java

---
+++ StringExample8.java
@@ -0,0 +1,7 @@
+public class StringExample8 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int index = str.indexOf('W');
+        System.out.println("Index of 'W': " + index);
+    }
+}
