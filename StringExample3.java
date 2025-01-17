259c68d9f160394f6daf365d9647f9976989561b
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:45:39 +0530

    Create StringExample3.java

---
+++ StringExample3.java
@@ -0,0 +1,7 @@
+public class StringExample3 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        boolean result = str.contains("World");
+        System.out.println("Contains 'World': " + result);
+    }
+}
