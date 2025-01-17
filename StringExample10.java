eece32a930a8503a36f325dd5f7a010c1ff6c41f
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:49:20 +0530

    Create StringExample10.java

---
+++ StringExample10.java
@@ -0,0 +1,7 @@
+public class StringExample10 {
+    public static void main(String[] args) {
+        String str = "";
+        boolean result = str.isEmpty();
+        System.out.println("String is empty: " + result);
+    }
+}
