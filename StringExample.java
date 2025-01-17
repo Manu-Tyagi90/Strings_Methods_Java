662c5f3e408ef8da611b63bbe898dc062a78942a
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:43:40 +0530

    Create StringExample.java

---
+++ StringExample.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char ch = str.charAt(7);
+        System.out.println("Character at index 7: " + ch);
+    }
+}
