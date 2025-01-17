3e8afdb4ad172c9e0622f2e40453f3aa7efb8452
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:54:42 +0530

    Create StringExample18.java

---
+++ StringExample18.java
@@ -0,0 +1,7 @@
+public class StringExample18 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String result = str.toLowerCase();
+        System.out.println("Lowercase string: " + result);
+    }
+}
