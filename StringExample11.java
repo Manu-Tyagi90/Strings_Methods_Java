6d882fc144df986b65a86e2c9ccd5441b57b68fe
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:50:11 +0530

    Create StringExample11.java

---
+++ StringExample11.java
@@ -0,0 +1,7 @@
+public class StringExample11 {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int length = str.length();
+        System.out.println("Length of string: " + length);
+    }
+}
