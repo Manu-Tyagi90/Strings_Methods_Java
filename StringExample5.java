077f9ac7c9caae1d1b5dcac6d8ae263ad4ab2b8f
Manu Tyagi <manutygii90@gmail.com>
2025-01-17 08:46:33 +0530

    Create StringExample5.java

---
+++ StringExample5.java
@@ -0,0 +1,8 @@
+public class StringExample5 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        boolean result = str1.equals(str2);
+        System.out.println("Strings are equal: " + result);
+    }
+}
